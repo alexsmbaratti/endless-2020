d968c91a43ad8515515f98b500ce3ee5b4d751d6
Alex Baratti <32379831+alexsmbaratti@users.noreply.github.com>
2022-11-25 20:35:04 -0800

    Initial commit

---
+++ main.py
@@ -0,0 +1,17 @@
+from datetime import date
+
+
+def calculate_date_delta(input_date):
+    """Calculates the number of days since December 31, 2020"""
+    end_of_2020 = date(2020, 12, 31)
+    delta = input_date - end_of_2020
+    return delta.days
+
+
+def format_date(date_delta):
+    day = 31 + date_delta  # 31 days in December
+    return f"December {day}, 2020"
+
+
+today = date.today()
+print(format_date(calculate_date_delta(today)))
