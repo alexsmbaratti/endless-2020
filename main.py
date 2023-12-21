685ce1b72d3324591b103382c9b5bec9715b1f1a
Alex Baratti <32379831+alexsmbaratti@users.noreply.github.com>
2023-12-20 22:42:58 -0800

    Adding typing

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
 from datetime import date
 
 
-def calculate_date_delta(input_date):
+def calculate_date_delta(input_date: date) -> int:
     """Calculates the number of days since December 31, 2020"""
     end_of_2020 = date(2020, 12, 31)
     delta = input_date - end_of_2020
     return delta.days
 
 
-def format_date(date_delta):
+def format_date(date_delta: int) -> str:
     day = 31 + date_delta  # 31 days in December
     return f"December {day}, 2020"
 
