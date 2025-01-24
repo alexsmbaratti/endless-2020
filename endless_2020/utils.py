0417ee3e05bd7761b04061ffb5a10f1f08fa1da9
Alex Baratti <32379831+alexsmbaratti@users.noreply.github.com>
2025-01-23 19:26:57 -0800

    Adds get_datetime

--- endless_2020/utils.py
+++ endless_2020/utils.py
@@ -1,4 +1,4 @@
-from datetime import datetime
+from datetime import datetime, timedelta
 
 from endless_2020.constants import END_OF_2020
 
@@ -7,3 +7,7 @@ def get_day_offset(input_date: datetime) -> int:
     """Calculates the number of days since December 31, 2020"""
     delta = input_date - END_OF_2020
     return delta.days
+
+
+def get_datetime(day_offset: int) -> datetime:
+    return END_OF_2020 + timedelta(days=day_offset)
