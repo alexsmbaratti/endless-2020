471c5d5d3f5555c9880827b2c4ac602261b31b5b
Alex Baratti <32379831+alexsmbaratti@users.noreply.github.com>
2025-01-24 23:30:52 -0800

    Adding license and setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="endless_2020",
+    version="0.1.0",
+    description="Hindsight is 20/20",
+    author="alexsmbaratti",
+    packages=find_packages(),
+    install_requires=[],
+)
